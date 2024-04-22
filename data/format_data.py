c5680b32ddd02970f4fab755d45417e70c9523b9
Elliott Arbogast <elliott.arbogast25@trinityschoolnyc.org>
2024-04-22 12:31:52 -0400

    calmchange

--- data/format_data.py
+++ data/format_data.py
@@ -1,7 +1,7 @@
 import json
 
 
-f1 = open("data/gasemissions.csv", "r")
+f1 = open("data/stateemissions.csv", "r")
 lines = f1.readlines()
 
 dictionary ={}
@@ -13,15 +13,12 @@ for lineIndex in range(1, len(lines)):
     if (splitLine[0] not in dictionary): 
         dictionary[splitLine[0]] = {}
     
-    dictionary[splitLine[0]]["MMBTU/metric ton steam"] = float(splitLine[1])
-    dictionary[splitLine[0]]["CO2 - kg per metric ton steam"] = float(splitLine[2]) 
-    dictionary[splitLine[0]]["CH4 - kg per metric ton steam"] = float(splitLine[3])
-    dictionary[splitLine[0]]["N2O - kg per metric ton steam"] = float(splitLine[4]) 
-    dictionary[splitLine[0]]["CO2e - kg per metric ton steam"] = float(splitLine[5]) 
+    dictionary[splitLine[0]] = float(splitLine[1])
+
 
 f1.close()
 
-f2 = open("emissions.json", "w")
+f2 = open("stateemissions.json", "w")
 json.dump(dictionary, f2, indent = 4)
 
 f2.close()
